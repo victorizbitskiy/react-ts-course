d4e05b1d31aee30b1e8efe8d52db4cdc800e9b28
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-06-28 21:27:20 +0300

    FC props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import Card from './components/Card';
+import React from "react";
+import Card, { CardVariant } from "./components/Card";
 
 const App = () => {
   return (
     <div>
-      <Card width='200px' height='200px' />
+      <Card variant={CardVariant.primary} width="200px" height="200px">
+        <button>Кнопка</button>
+        <div>Текст</div>
+      </Card>
     </div>
   );
 };
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { FC } from "react";
 
+export enum CardVariant {
+  outlined = "outlined",
+  primary = "primary",
+}
 interface CardProps {
   width?: string;
   height?: string;
+  variant: CardVariant;
+  children: React.ReactNode;
 }
 
-const Card = ({width, height}: CardProps) => {
+const Card: FC<CardProps> = ({ width, height, variant, children }) => {
   return (
-    <div style={{width, height, border: '1px solid gray'}}>
-      
+    <div
+      style={{
+        width,
+        height,
+        border: variant === CardVariant.outlined ? "1px solid gray" : "none",
+        background: variant === CardVariant.primary ? "lightgray" : "",
+      }}
+    >
+      {children}
     </div>
   );
 };
