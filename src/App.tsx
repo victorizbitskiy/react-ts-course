0b9a53e4e6892b9de097fd06a90cd9b1a40e8c3e
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-07-08 21:09:20 +0300

    useParams

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserPage from "./components/UserPage";
 import TodosPage from "./components/TodosPage";
+import { NavLink } from "react-router-dom";
+import UserItemPage from "./components/UserItemPage";
+import TodoItemPage from "./components/TodoItemPage";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <div>
+        <NavLink to={"/users"}>Пользователи</NavLink>
+        <NavLink to={"/todos"}>Список дел</NavLink>
+      </div>
       <Routes>
         <Route path="/users" element={<UserPage />} />
         <Route path="/todos" element={<TodosPage />} />
+        <Route path="/users/:id" element={<UserItemPage />} />
+        <Route path="/todos/:id" element={<TodoItemPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/UserItemPage.tsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import React, { FC, useEffect, useState } from "react";
+import { IUser } from "../types/types";
+import { useParams } from "react-router-dom";
+
+type UserItemPageParams = {
+  id: string;
+}
+
+const UserItemPage: FC = () => {
+  const [user, setUser] = useState<IUser | null>(null);
+  const params = useParams<UserItemPageParams>();
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  async function fetchUser() {
+    try {
+      const response = await axios.get<IUser>(
+        "https://jsonplaceholder.typicode.com/users/" + params.id 
+      );
+      setUser(response.data);
+    } catch (e) {
+      alert(e);
+    }
+  }
+
+  return (
+    <div>
+      <button>Back</button>
+      <h1>Страница пользователя {user?.name}</h1>
+      <div>
+        {user?.email}
+      </div>
+      <div>
+        {user?.address.city} {user?.address.street} {user?.address.zipcode}
+      </div>
+    </div>
+  );
+};
+
+export default UserItemPage;
